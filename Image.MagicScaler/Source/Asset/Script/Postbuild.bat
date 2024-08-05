6171f4f7301c92d1af2e833f60a807736a4bab6b
FBiDev <fernando.birnfeld@gmail.com>
2024-08-05 12:14:37 -0300

    Remove Folder Plurals, Remade names

--- Image.MagicScaler/Source/Asset/Script/Postbuild.bat
+++ Image.MagicScaler/Source/Asset/Script/Postbuild.bat
@@ -1,4 +1,10 @@
 echo -------------------------------------------------------------------------------
 echo    App.Image.MagicScaler     ::     Post-buid - Copy DLL
-robocopy ".\ " "..\..\..\Bin " *.dll *.pdb *.xml /XO
+
+set ConfigurationName=%1
+if %ConfigurationName% == Release (
+robocopy ".\ " "..\..\..\Bin " *.dll /XO
+) else (
+robocopy ".\ " "..\..\..\Bin " *.dll *.pdb *.xml /XO )
+
 if %errorlevel% leq 4 echo App.Image.MagicScaler Post-buid ExitCode: %errorlevel%&echo:& exit 0 else exit %errorlevel%
