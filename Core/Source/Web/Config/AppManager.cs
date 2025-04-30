13d31da049b047a3bd88e8a64b4dade3ff517908
FBiDev <68982125+FBiDev@users.noreply.github.com>
2025-04-30 02:05:49 -0300

    add Web AppManager Name

--- Core/Source/Web/Config/AppManager.cs
+++ Core/Source/Web/Config/AppManager.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.IO;
+using System.Web;
 
 namespace App.Core.Web
 {
     public static class AppManager
     {
+        public static string Name
+        {
+            get { return HttpContext.Current.Request.ApplicationPath; }
+        }
+
         public static string BaseDirectory
         {
             get { return AppDomain.CurrentDomain.BaseDirectory; }
@@ -12,7 +18,7 @@ namespace App.Core.Web
 
         public static string GetBaseFile(string filepath)
         {
-             return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, filepath); 
+            return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, filepath);
         }
     }
 }
